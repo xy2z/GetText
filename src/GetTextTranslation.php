a660e2ef4f22dfd8634d714fed7672d3c1a99f2b
xy <xyzbk89@gmail.com>
2018-01-08 20:20:23 +0100

    Documentation in GetTexTranslation

--- src/GetTextTranslation.php
+++ src/GetTextTranslation.php
@@ -10,7 +10,7 @@
 		 *
 		 * Label + instance_key each have a translation.
 		 *
-		 * @var [type]
+		 * @var string
 		 */
 		private $label;
 
@@ -28,25 +28,44 @@
 		 */
 		private $translation;
 
-
+		/**
+		 * Constructor
+		 *
+		 * @param string $label Translation label
+		 * @param string|int $instance_key The instance ID/key.
+		 * @param string $translation Translated text.
+		 */
 		public function __construct(string $label, $instance_key, string $translation) {
 			$this->label = $label;
 			$this->instance_key = $instance_key;
 			$this->translation = $translation;
 		}
 
-
-		public function get_label() {
+		/**
+		 * Get label
+		 *
+		 */
+		public function get_label() : string {
 			return $this->label;
 		}
 
-
+		/**
+		 * Get $instance_key
+		 *
+		 */
 		public function get_instance_key() {
 			return $this->instance_key;
 		}
 
-
-		public function get_translation(array $replacements, bool $empty_if_untranslated) {
+		/**
+		 * Get translated text with replacements
+		 *
+		 * @param array $replacements Replacements without { or }.
+		 * @param bool $empty_if_untranslated If translation doesn't exists.
+		 *
+		 * @return string The translated text.
+		 */
+		public function get_translation(array $replacements, bool $empty_if_untranslated) : string {
 			$translation = $this->translation;
 
 			// Replacements.
